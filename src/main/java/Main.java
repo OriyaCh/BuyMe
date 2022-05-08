44f9f5f6b70bfbbfc2c90f461cda8cf7745a9698
dgotlieb <Dgotlieb8@gmail.com>
2022-05-08 18:04:09 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
@@ -8,18 +10,16 @@ public class Main {
     private static WebDriver driver;
 
 
-    public static void main(String[] args) {
+    @BeforeClass
+    public static void beforeAll() {
         System.setProperty("webdriver.chrome.driver", Constants.CHROMEDRIVER_PATH);
         driver = new ChromeDriver();
         driver.get("https://www.buyme.co.il/");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
-     public void test01_clickandfindElementBylink ()
-
-         {driver.findElement(By.linkText("כניסה / הרשמה")).click();}
-
-
-
-
+    @Test
+    public void test01_clickandfindElementBylink() {
+        driver.findElement(By.linkText("כניסה / הרשמה")).click();
+    }
 }
